2fb550643bae0dfaca24aaa45eb4d26eccf520f7
Ebimary <igwemary220@gmail>
2022-12-09 15:11:28 -0800

    base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
@@ -8,3 +7,19 @@
  * Return: Always 0 (Success)
  */
 int main(void)
+{
+	int num = '0'; /*Initialize num to 0*/
+	char letter = 'a'; /*Initialize letter to a*/
+
+	while (num <= '9') /*print 0-9*/
+	{
+		putchar(num);
+		num++;
+	}
+	while (letter <= 'f') /*print a-f*/
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
