473fddb63b3289f5fc0d3173a626afa68664ce05
Ebimary <igwemary220@gmail>
2022-12-09 15:58:56 -0800

    comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,12 +1,28 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - prints numbers from 0 to 9 separated by a coma followed by a space
  *
  * Return: Always 0 (Success)
  */
 int main(void)
+{
+	int num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		if (num != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
+
+
 
 
