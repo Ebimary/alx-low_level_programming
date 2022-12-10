f5265973270a4f53c3f5c85deea0382dce56dd3e
Ebimary <igwemary220@gmail>
2022-12-09 19:40:58 -0800

    coding c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible differnt combinations of two digits
+ * main - prints all possible different combinations of two digits
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int ones = '0';
-	    int tens = '0';
+	int tens = '0';
 
-	    for (tens = '0'; tens <= '9'; tens++)/*Print tens digit*/
-	    {
-		   	 for (ones = '0'; ones <= '9'; ones++)/*print ones digit*/
-		    {
-			    if (!((ones == tens) || (tens > ones)))/*eleminates repitition*/
-			    {
-				    putchar(tens);
-				    putchar(ones);
-				    if (!(ones == '9' && tens == '8'))/*addes command space*/
-				    {
-					    putchar(',');
-					    putchar(' ');
-				    }
-			    }
-		    }
-	    }	   
-	    putchar('\n');
-	    return (0);
+	for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
+	{
+		for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
+		{
+			if (!((ones == tens) || (tens > ones)))/* eleminates repitition*/
+			{
+				putchar(tens);
+				putchar(ones);
+				if (!(ones == '9' && tens == '8'))/* addes command space*/
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
