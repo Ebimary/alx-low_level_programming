fba663936f1f65f16978357c2e6393a772e87445
Ebimary <igwemary220@gmail.com>
2023-03-16 09:17:13 -0700

    print dlistint

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,8 +11,8 @@ size_t print_dlistint(const dlistint_t *h)
 size_t i;
 for (i = 0; h != NULL; i++)
 {
- printf("%d\n", h->n);
- h = h->next;
+	printf("%d\n", h->n);
+	h = h->next;
 }
 return (i);
 }
